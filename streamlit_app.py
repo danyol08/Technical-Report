4dcec0efad1814b519aceca0c3f8b18488a52399
Daniel Cruz <132664877+danyol08@users.noreply.github.com>
2025-09-05 11:59:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,21 @@ import requests
 from authlib.integrations.requests_client import OAuth2Session
 from streamlit_gsheets import GSheetsConnection
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
 # --- Streamlit Config ---
 st.set_page_config(layout="wide")
 st.title("Technical Reports - 2025")
@@ -179,9 +194,4 @@ if "token" in st.session_state:
             st.dataframe(display_df, use_container_width=True, height=700, hide_index=True)
         else:
             st.info("No reports yet. Start adding using the form on the left.")
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """ 
-#GithubIcon {   visibility: hidden;
-} 
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
